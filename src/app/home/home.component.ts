e07fea4ad15bc336e7b0456aa2161325f0dac219
michirem <dariuss@gmx.at>
2021-03-23 11:16:06 +0100

    created base card structure for home page (cars) and added objectr array (cars) to home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,46 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cars = [{
+    "carName": "Equinox",
+    "carType": "Chevrolet",
+    "carColor": "Orange",
+    "carPrice": 27748
+  }, {
+    "carName": "Scirocco",
+    "carType": "Volkswagen",
+    "carColor": "Puce",
+    "carPrice": 24218
+  }, {
+    "carName": "Explorer Sport Trac",
+    "carType": "Ford",
+    "carColor": "Blue",
+    "carPrice": 18512
+  }, {
+    "carName": "Windstar",
+    "carType": "Ford",
+    "carColor": "Khaki",
+    "carPrice": 28512
+  }, {
+    "carName": "5 Series",
+    "carType": "BMW",
+    "carColor": "Puce",
+    "carPrice": 31982
+  }, {
+    "carName": "GS",
+    "carType": "Lexus",
+    "carColor": "Red",
+    "carPrice": 17518
+  }, {
+    "carName": "Intrigue",
+    "carType": "Oldsmobile",
+    "carColor": "Yellow",
+    "carPrice": 19208
+  }, {
+    "carName": "88",
+    "carType": "Oldsmobile",
+    "carColor": "Puce",
+    "carPrice": 29580
+  }];
+
 }
